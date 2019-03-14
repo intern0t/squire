950231fae440317e0e678302b5faebeb7a4d332a
Prashant Shrestha <intern0t@users.noreply.github.com>
2019-03-14 19:23:50 -0400

    Test data for the table, added user field.
    Signed-off-by: Prashant Shrestha <intern0t@users.noreply.github.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-	return <div className="footer">Footer</div>;
+	return <div className="footer">Copyright &copy; 2019, Squire</div>;
 };
 
 export default Footer;
--- src/components/Tabular.js
+++ src/components/Tabular.js
@@ -3,11 +3,76 @@ import Icon from "./Icon";
 
 class Tabular extends Component {
 	state = {
-		fakeData: []
+		fakeData: [
+			{
+				user: "Random User",
+				projectTitle: "Random Project Title",
+				projectDescription: "Random Project Description",
+				repoLink: "https://beanstalk.com/",
+				majorLanguage: ["javascript"],
+				privacy: Math.round(Math.random()),
+				dateAdded: "Mar 13 16:29:35 EDT 2019"
+			},
+			{
+				user: "Random User",
+				projectTitle: "Random Project Title",
+				projectDescription: "Random Project Description",
+				repoLink: "https://github.com/",
+				majorLanguage: ["react"],
+				privacy: Math.round(Math.random()),
+				dateAdded: "Mar 13 16:29:35 EDT 2019"
+			},
+			{
+				user: "Random User",
+				projectTitle: "Random Project Title",
+				projectDescription: "Random Project Description",
+				repoLink: "https://bitbucket.com/",
+				majorLanguage: ["javascript"],
+				privacy: Math.round(Math.random()),
+				dateAdded: "Mar 13 16:29:35 EDT 2019"
+			},
+			{
+				user: "Random User",
+				projectTitle: "Random Project Title",
+				projectDescription: "Random Project Description",
+				repoLink: "https://mercurial.com/",
+				majorLanguage: ["ruby"],
+				privacy: Math.round(Math.random()),
+				dateAdded: "Mar 13 16:29:35 EDT 2019"
+			},
+			{
+				user: "Random User",
+				projectTitle: "Random Project Title",
+				projectDescription: "Random Project Description",
+				repoLink: "https://codecommit.com/",
+				majorLanguage: ["python"],
+				privacy: Math.round(Math.random()),
+				dateAdded: "Mar 13 16:29:35 EDT 2019"
+			},
+			{
+				user: "Random User",
+				projectTitle: "Random Project Title",
+				projectDescription: "Random Project Description",
+				repoLink: "https://subversion.com/",
+				majorLanguage: ["c#"],
+				privacy: Math.round(Math.random()),
+				dateAdded: "Mar 13 16:29:35 EDT 2019"
+			},
+			{
+				user: "Random User",
+				projectTitle: "Random Project Title",
+				projectDescription: "Random Project Description",
+				repoLink: "https://gitlab.com/",
+				majorLanguage: ["nodejs"],
+				privacy: Math.round(Math.random()),
+				dateAdded: "Mar 13 16:29:35 EDT 2019"
+			}
+		]
 	};
 
 	render() {
 		const { title } = this.props;
+		const { fakeData } = this.state;
 
 		return (
 			<div className="tabular">
@@ -15,6 +80,7 @@ class Tabular extends Component {
 				<table>
 					<thead>
 						<tr>
+							<th>User</th>
 							<th>Project Title</th>
 							<th>Description</th>
 							<th>Repository Link</th>
@@ -24,108 +90,38 @@ class Tabular extends Component {
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td>1</td>
-							<td>1</td>
-							<td>
-								<Repository
-									type="github"
-									link="https://beanstalk.com/"
-								/>
-							</td>
-							<td>
-								<Language language="javascript" />
-							</td>
-							<td>
-								<Privacy type={0} />
-							</td>
-							<td>Wed Mar 13 16:29:35 EDT 2019</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>1</td>
-							<td>
-								<Repository
-									type="github"
-									link="https://github.com/"
-								/>
-							</td>
-							<td>
-								<Language language="react" />
-							</td>
-							<td>
-								<Privacy type={1} />
-							</td>
-							<td>Wed Mar 13 16:29:35 EDT 2019</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>1</td>
-							<td>
-								<Repository
-									type="gitlab"
-									link="https://gitlab.com/"
-								/>
-							</td>
-							<td>
-								<Language language="python" />
-							</td>
-							<td>
-								<Privacy type={0} />
-							</td>
-							<td>Wed Mar 13 16:29:35 EDT 2019</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>1</td>
-							<td>
-								<Repository
-									type="codecommit"
-									link="https://codecommit.com/"
-								/>
-							</td>
-							<td>
-								<Language language="ruby" />
-							</td>
-							<td>
-								<Privacy type={1} />
-							</td>
-							<td>Wed Mar 13 16:29:35 EDT 2019</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>1</td>
-							<td>
-								<Repository
-									type="bitbucket"
-									link="https://bitbucket.com/"
-								/>
-							</td>
-							<td>
-								<Language language="php" />
-							</td>
-							<td>
-								<Privacy type={0} />
-							</td>
-							<td>Wed Mar 13 16:29:35 EDT 2019</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>1</td>
-							<td>
-								<Repository
-									type="mercurial"
-									link="https://mercurial.com/"
-								/>
-							</td>
-							<td>
-								<Language language="java" />
-							</td>
-							<td>
-								<Privacy type={0} />
-							</td>
-							<td>Wed Mar 13 16:29:35 EDT 2019</td>
-						</tr>
+						{fakeData && fakeData.length > 0
+							? fakeData.map(data => {
+									return (
+										<tr>
+											<td>{data.user}</td>
+											<td title={data.projectTitle}>
+												{data.projectTitle}
+											</td>
+											<td title={data.projectDescription}>
+												{data.projectDescription}
+											</td>
+											<td>
+												<Repository
+													link={data.repoLink}
+												/>
+											</td>
+											<td>
+												<Language
+													language={
+														data.majorLanguage[0] ||
+														"Unknown"
+													}
+												/>
+											</td>
+											<td>
+												<Privacy type={data.privacy} />
+											</td>
+											<td>{data.dateAdded}</td>
+										</tr>
+									);
+							  })
+							: null}
 					</tbody>
 				</table>
 			</div>
@@ -200,16 +196,37 @@ var generateRandomColor = () => {
 
 const Language = ({ language }) => {
 	var generatedColor = generateRandomColor();
+	const languagesAndIcons = {
+		javascript: "js",
+		ruby: "gem",
+		unknown: "code"
+	};
+	var formattedLanguage =
+		Object.entries(languagesAndIcons).find(data => {
+			if (data[0].toLowerCase() === language.toLowerCase()) {
+				return data[1];
+			}
+		}) ||
+		language ||
+		"code";
+
+	console.log(formattedLanguage);
 
 	return (
 		<div
 			className={`language language-${language}`}
 			style={{
-				backgroundColor: `rgba(${generatedColor}, 0.1)`,
+				backgroundColor: `rgba(${generatedColor}, 0.2)`,
 				color: `rgb(${generatedColor})`
 			}}
 		>
-			<Icon icon={`fab fa-${language.toLowerCase()}`} />
+			<Icon
+				icon={`${
+					formattedLanguage === "code"
+						? "fas fa-code"
+						: `fab fa-${formattedLanguage}`
+				}`}
+			/>
 			{language.toUpperCase() || "Unknown"}
 		</div>
 	);
